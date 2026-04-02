498cc86bfa00ea6f376794eea3fa5b3a3306a1a4
jeffyanta <jeff@flipcash.com>
2026-04-02 11:53:07 -0400

    Update MAX_SWAP_DOLLARS to $2000

--- api/src/consts.rs
+++ api/src/consts.rs
@@ -4,4 +4,4 @@ pub const VAULT: &[u8] = b"vault";
 pub const MAX_NAME_LEN: usize = 32;
 
 /// Maximum swap amount in whole dollars (e.g., 1000 = $1000)
-pub const MAX_SWAP_DOLLARS: u64 = 1000;
+pub const MAX_SWAP_DOLLARS: u64 = 2000;
